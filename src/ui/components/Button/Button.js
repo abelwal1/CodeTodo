453cb7b0933f5825ee20dbff6b24797c7e5413a0
Amit Belwal <technicalconsultant2u@gmail.com>
2023-02-24 00:02:31 +0530

    Added Color to all three buttons

--- src/ui/components/Button/Button.js
+++ src/ui/components/Button/Button.js
@@ -2,18 +2,26 @@ import React from "react";
 import {buttonClassType} from "../../../utils/constant";
 import $ from "./Button.module.css";
 
+const getButtonClass = (btnClassType)=>{
+let btnClassName= $.button;
+if(btnClassType === buttonClassType.default){ btnClassName+= " "+$.default; }
+else if(btnClassType === buttonClassType.primary){ btnClassName+= " "+ $.primary; }
+else if(btnClassType === buttonClassType.secondary){ btnClassName+= " "+ $.secondary; }
+return btnClassName;
+}
+
 const Button = ({
   children,
   onClick,
   type = "button",
-  variant = buttonClassType.primary, // or 'secondary'
+  variant, 
 }) => {
   return (
     <button
       // TODO: Add conditional classNames
       // - Must have a condition to set the '.primary' className
       // - Must have a condition to set the '.secondary' className
-      className={variant = buttonClassType.primary ?  $.button +" "+ $.primary : $.button + $.secondary }
+      className={getButtonClass(variant)}
       type={type}
       onClick={onClick}
     >
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,3 +1,3 @@
 import { createEnum } from "./common";
 
-export const buttonClassType = createEnum(['primary', 'secondary']);
+export const buttonClassType = createEnum(['primary', 'secondary', 'default']);
