2ff8e3642fbf1afaa22a8bbfc99c671cef339d5f
Amit Belwal <technicalconsultant2u@gmail.com>
2023-02-23 23:25:35 +0530

    Add the 'Roboto' font from Google fonts and add it as a global CSS var called --font-primary.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+// pages/_app.js
+//Another way of adding Google fonts
+// import { Roboto } from '@next/font/google'
+
+// const roboto = Roboto({
+//   weight: '400',
+//   subsets: ['latin'],
+// })
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return (
+//     <>
+//       <style jsx global>{`
+//         :root {
+//           --roboto-font: ${roboto.style.fontFamily};
+//         }
+//       `}</style>
+//       <Component {...pageProps} />
+//     </>
+//   );
+// }
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} 
+  // className={roboto.className}
+   />;
 }
